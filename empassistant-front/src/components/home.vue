1cebd8b5bb3c4d421445b2d96a23b0a102c709ea
lijiping8080 <2963930142@qq.com>
2019-05-08 22:36:47 +0800

    登录页

--- empassistant-front/src/components/home.vue
+++ empassistant-front/src/components/home.vue
@@ -1,1 +1,141 @@
- 
+ <template>
+  <div class="warp">
+    <div class="header">
+      <div class="container">
+        <h1>实习助手</h1>
+        <ul>
+          <li>
+            <a href="#">首页</a>
+          </li>
+          <li>
+            <a href="#">我的简历</a>
+          </li>
+          <li>
+            <a href="#">校园招聘会</a>
+          </li>
+          <li>
+            <a href="#">藏经阁</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="login-box">
+      <div class="login-title">学生登录</div>
+      <input type="text" id="username" placeholder="请输入账号" autofocus>
+      <input type="password" id="password" placeholder="请输入密码">
+      <div class="btn">
+        <a href="#" class="forget">忘记密码</a>
+        <a href="#" class="register">立即注册</a>
+      </div>
+      <div class="submit">立即登录</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped>
+.warp {
+  overflow: hidden;
+  width: 100%;
+  height: 100vh;
+  background-image: url("../assets/bg.png");
+  background-repeat: no-repeat;
+  background-size: 100%;
+}
+.header {
+  width: 100%;
+  height: 56px;
+  position: relative;
+  margin-top: 50px;
+}
+.container {
+  widows: 100%;
+  height: 100%;
+  max-width: 1080px;
+  margin: 0px auto;
+}
+.container h1 {
+  display: inline-block;
+  position: absolute;
+  top: -9999px;
+}
+.container ul {
+  list-style: none;
+  height: 100%;
+}
+.container ul li {
+  height: 100%;
+  float: left;
+}
+.container ul li a {
+  list-style: none;
+  text-decoration: none;
+  display: inline-block;
+  vertical-align: top;
+  color: #fff;
+  font-size: 16px;
+  padding: 0 16px;
+  height: 56px;
+  line-height: 56px;
+  margin: 0;
+}
+.login-box {
+  width: 220px;
+  height: 265px;
+  background-color: #fff;
+  margin-top: 60px;
+  padding: 30px 40px 50px;
+  z-index: 100;
+  position: absolute;
+  right: 20%;
+}
+.login-title{
+  text-align: center;
+  font-size: 20px;
+  color: #0287ee;
+}
+.login-box input{
+  margin-top: 20px;
+  width: 100%;
+  height: 25px;
+  line-height: 25px;
+  font-size: 14px;
+  padding: 10px 0;
+  color: #262626;
+  border: none;
+  outline: none;
+  border-bottom: 1px solid #0287ee;
+}
+.forget{
+  text-decoration: none;
+  display: inline-block;
+  color: rgba(38,38,38,.85);
+  font-size: 14px;
+  margin: 20px 50px 20px 5%;
+}
+.register{
+  text-decoration: none;
+  display: inline-block;
+  color: #0287ee;
+  font-size: 14px;
+  margin: 20px 5% 20px 0px;
+}
+.submit{
+  font-size: 12px;
+  color: #fff;
+  width: 100%;
+  height: 38px;
+  line-height: 38px;
+  text-align: center;
+  background-color: #0287ee;
+  cursor: pointer;
+}
+</style>
