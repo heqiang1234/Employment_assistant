823e9de14ab58f6da19dc7b759e0538f09b54204
wuyunzhemu <415159489@qq.com>
2019-05-08 20:31:14 +0800

    test

--- empassistant-front/src/App.vue
+++ empassistant-front/src/App.vue
@@ -7,17 +7,9 @@
 
 <script>
 export default {
-  name: '就业助手'
+ 
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
