cb20249f94d480a07f9959e54398bbd5a23f3dc4
lijiping8080 <2963930142@qq.com>
2019-05-16 08:39:40 +0800

    change

--- empassistant-front/src/components/home.vue
+++ empassistant-front/src/components/home.vue
@@ -1,4 +1,4 @@
- <template>
+<template>
   <div class="warp">
     <div class="header">
       <div class="container">
@@ -17,54 +17,63 @@
             <a href="#">藏经阁</a>
           </li>
         </ul>
-      </div> 
-    </div>
-    <div class="login-box">
-      <div class="login-title">学生登录</div>
-      <input type="text" id="username" placeholder="请输入账号" autofocus>
-      <input type="password" id="password" placeholder="请输入密码">
-      <div class="btn">
-        <a href="#" class="forget">忘记密码</a>
-        <a href="http://localhost:8080/#/register" class="register">立即注册</a>
       </div>
-      <div class="submit">立即登录</div>
+    </div>    
+    <div class="top">
+        <div class="container">
+            <div class="top-leftImg">
+                <img class="leftImg" src="../assets/main-picture.png" alt="">
+            </div>
+        </div>
     </div>
-    <div class="login-bottom">
-      <p>就业助手你的好帮手</p>
-      <span>国内领先的实习生招聘平台</span>
+    <div class="top-bar">
+        <div class="container">
+            <div class="left-bar">
+                <div class="user-count">0018983</div>
+                <div class="user-count-num">共计0018983个热门职业等着你</div>
+            </div>
+            <div class="right-bar">
+                <div class="form">
+                    <div class="bnt1">全国 <img src="../assets/arrow1.png" alt=""></div>
+                    <div class="bnt1 bnt2">搜职业 <img src="../assets/arrow1.png" alt=""></div>
+                    <div class="bnt3"><input class="search" type="text"  placeholder="请输入您期望的职位" autofocus></div>
+                    <div class="bnt4">立即搜索</div>
+                </div>
+                <div class="form-bar">
+                    热门搜索：
+                    <a href="">项目</a>
+                    <a href="">前端</a>
+                    <a href="">财会</a>
+                    <a href="">市场</a>
+                    <a href="">行政</a>
+                    <a href="">广告</a>
+                </div>
+            </div>
+        </div>
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: "HelloWorld",
-  data() {
-    return {};
-  }
-};
-</script>
-
-<style scoped>
+<style scope>
+*{
+    margin: 0px;
+    padding: 0px;
+}
 .warp {
-  overflow: hidden;
   width: 100%;
-  height: 100vh;
-  background-image: url("../assets/bg.png");
-  background-repeat: no-repeat;
-  background-size: 100%;
+  min-width: 1080px;
+  background: #fefefe;
 }
 .header {
   width: 100%;
   height: 56px;
-  position: relative;
-  margin-top: 50px;
+  border-bottom: 1px solid #333;
 }
 .container {
-  widows: 100%;
   height: 100%;
-  max-width: 1080px;
+  max-width: 90%;
   margin: 0px auto;
+  position: relative;
 }
 .container h1 {
   display: inline-block;
@@ -84,85 +93,145 @@ export default {
   text-decoration: none;
   display: inline-block;
   vertical-align: top;
-  color: rgba(255,255,255,.8);
+  color: rgba(3, 3, 3, 0.8);
   font-size: 16px;
   padding: 0 16px;
-  height: 56px;
   line-height: 56px;
-  margin: 0;
   cursor: pointer;
 }
-.container ul li a:hover{
-  color: #fff;
+.container ul li a:hover {
+  color: #02a0e9;
 }
-.login-box {
-  width: 220px;
-  height: 265px;
-  background-color: #fff;
-  margin-top: 60px;
-  padding: 30px 40px 50px;
-  z-index: 100;
-  position: absolute;
-  right: 20%;
+.top{
+    width: 100%;
+    min-width: 1080px;
+    height: 172px;
+    background-color: #02a0e9;
 }
-.login-title{
-  text-align: center;
-  font-size: 20px;
-  color: #0287ee;
+.top-leftImg{
+    width: 260px;
+    height: 100%;
+    display: inline-block;
+    position: relative;
 }
-.login-box input{
-  margin-top: 20px;
-  width: 100%;
-  height: 25px;
-  line-height: 25px;
-  font-size: 14px;
-  padding: 10px 0;
-  color: #262626;
-  border: none;
-  outline: none;
-  border-bottom: 1px solid #0287ee;
+.leftImg{
+    position: absolute;
+    bottom: 0px;
+    left: 22px;
+    display: block;
 }
-.forget{
-  text-decoration: none;
-  display: inline-block;
-  color: rgba(38,38,38,.85);
-  font-size: 14px;
-  margin: 20px 50px 20px 5%;
+.top-bar{
+    width: 100%;
+    padding-top: 20px;
+    height: 120px;
+    box-sizing: border-box;
+    position: absolute;
+    left: 0px;
 }
-.register{
-  text-decoration: none;
-  display: inline-block;
-  color: #0287ee;
-  font-size: 14px;
-  margin: 20px 5% 20px 0px;
+.left-bar{
+    width: 25%;
+    height: 100px;
+    padding-top: 20px;
+    background-color: #eee;
+    display: inline-block;
+    box-sizing:border-box;
 }
-.submit{
-  font-size: 12px;
-  color: #fff;
-  width: 100%;
-  height: 38px;
-  line-height: 38px;
-  text-align: center;
-  background-color: #0287ee;
-  cursor: pointer;
+.user-count{
+    width: 234px;
+    height: 50px;
+    box-sizing:border-box;
+    line-height: 30px;
+    margin: 0px auto;
+    padding: 10px 0px;
+    text-align: center;
+    font-size: 28px;
+    color: #02a0e9;
+    background-color: #fafafa;
 }
-.login-bottom{
-  position: absolute;
-  width: 100%;
-  bottom: 10%;
-  text-align: center;
+.user-count-num{
+    width: 100%;
+    line-height: 24px;
+    font-size: 12px;
+    text-align: center;
+    height: 24px;
+    color: #333;
 }
-.login-bottom p{
-  font-family: "微软雅黑";
-  font-size: 36px;
-  font-weight: bold;
-  color: #595959;
-  display: block;
+.right-bar{
+    width: 73%;
+    height: 100px;
+    padding-top: 20px;
+    display: inline-block;
+    box-sizing:border-box;
+    position: absolute;
+    right: 0px;
 }
-.login-bottom span{
-  margin-top: 10px;
-  font-size: 14px;
-  color: rgba(38,38,38,.5);
-  display: block;
+.form{
+    display: flex;
+    align-items: center;
+    width: 100%;
+    height: 46px;
+    border: 2px solid #02a0e9;
+    /* box-sizing:border-box; */
+    font-size: 14px;
+    font-family: "微软雅黑";
+}
+.bnt1{
+    display: inline-block;
+    width: 10%;
+    box-sizing:border-box;
+    text-align: center;
+    cursor: pointer;
+    border-right: 1px solid #02a0e9;
+}
+.bnt2{
+    width: 11%;
+}
+.bnt3{
+    width: 50%;
+    box-sizing:border-box;
+    padding-left:12px; 
+    display: inline-block;
+}
+.search{
+    border: none;
+    outline: none;
+    display: inline-block;
+    width: 398px;
+    margin: 0px;
+    padding: 0px;
+    font-size: 16px;
+}
+.bnt4{
+    cursor: pointer;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    flex: 1;
+    /* box-sizing:border-box; */
+    background:#02a0e9;
+    color: #fafafa;
+    font-size: 16px;
+    display: flex;
+}
+.bnt4:hover{
+    background: #abcdef;
+}
+.form-bar{
+    width: 100%;
+    height: 24px;
+    margin-top: 5px; 
+    color: #6aa2e4;
+    text-decoration: none;
+    line-height: 28px;
+    font-size: 14px;
+}
+.form-bar>a{
+    color: #6aa2e4;
+    padding: 0px 5px;
+    text-decoration: none;
+    font-size: 14px;
+}
+.form-bar>a:hover{
+    text-decoration: underline;
 }
 </style>
--- empassistant-front/src/router/index.js
+++ empassistant-front/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import home from '@/components/home'
 import register from '@/components/register'
+import login from '@/components/login'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
       path: '/register',
       name: 'register',
       component: register
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: login
     }
   ]
 })
